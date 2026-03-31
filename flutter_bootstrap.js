ea718941d4ce52e5c49a0768b54a1980a77f3c67
mark-aique <mark.cabalona@aiqueinnovation.com>
2026-03-31 18:37:38 +0800

    feat: turn off network logs in web app

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1154906143"
+    serviceWorkerVersion: "2776451790"
   }
 });
